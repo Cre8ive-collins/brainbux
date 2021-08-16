004ed391fac8706bb2801a3e1fcc80c4e09d5f4c
Cre8ive-collins <wilsoncollins44@yahoo.com>
2021-08-16 08:47:01 +0100

    homely

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FootComponent } from './foot/foot.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    FootComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
